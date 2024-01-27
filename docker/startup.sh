ccb454250c56fae8dbe1752af82d763657fcc05b
Ramya <ramya.bala221190@gmail.com>
2024-01-27 13:49:28 +0530

    updated config files

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #cp /usr/share/nginx/html/assets/${env}-config.js /usr/share/nginx/html/assets/config.js
-echo "Starting container. Runtime env variables: config=${config}"
+echo "Starting container. Runtime env variables: env=${env} var1=${var1}"
 nginx -g 'daemon off;'
