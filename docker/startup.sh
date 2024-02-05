c15c36df3479e1c2c43bd04a87eb0fba37c38702
Ramya <ramya.bala221190@gmail.com>
2024-02-05 21:12:23 +0530

    updated config

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cp /usr/share/nginx/html/assets/json/config-${env}.json /usr/share/nginx/html/assets/json/config.json
-# cat /usr/share/nginx/html/assets/js/config-${env}.js
-cp /usr/share/nginx/html/assets/js/config-${env}.js /usr/share/nginx/html/assets/js/config.js
-echo "Starting container. Runtime env variables: env=${env} var1=${var1}"
+echo "Starting container. Runtime environment variables.
+1) environment from kube config file=${env} 2) environment from dockerfile=${dockerEnv}"
 nginx -g 'daemon off;'
