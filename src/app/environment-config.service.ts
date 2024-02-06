d37a80006b345567a3cd3df2906af5ba2dbb7238
Ramya <ramya.bala221190@gmail.com>
2024-02-06 20:55:08 +0530

    added import

--- src/app/environment-config.service.ts
+++ src/app/environment-config.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
+import * as configJson from '../assets/json/config.json';
+
 export interface environmentConfig{
   env:string|null,
   baseUrl:string,
@@ -23,6 +25,7 @@ export class EnvironmentConfigService {
   }
 
   loadConfig(){
+    console.log("Imported config.json",configJson)
      console.log("Checking contents of environment.ts file : ",environment);
      return this.http.get('/assets/json/config.json').pipe(
       tap((config:any)=>{
